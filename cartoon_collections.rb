5680c528d4168b6ae3acf3a26598d62fdd14d7e2
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-03-15 21:43:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,4 +7,21 @@ def roll_call_dwarves(dwarves)
   puts new_dwarves.join(" ")
 end
 
-def summon_captain_planet
+def summon_captain_planet(array)
+  array.collect do |names|
+    names.capitalize << "!"
+  end
+end
+
+def long_planeteer_calls(array)
+  array.any? do |words|
+    words.size > 4
+  end
+end
+
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda" "camembert"]
+  array.find do |cheese|
+    cheese_types.include?(cheese)
+  end
+end
